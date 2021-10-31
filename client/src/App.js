5cf10bdbc50626340de5cfdddcadf25163d2cccc
confusedDip <anihero4@gmail.com>
2021-10-31 13:45:12 +0530

    Client:Added bootstrap, reactstrap, react-poppers

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react';
+import './App.css';
+import {Container} from 'reactstrap';
 
-function App(){
-        return(
-                <div>
-                        <h1>Online Document Tracking System</h1>
-                        <p>Developed by: Souradip Nath, Soumili Bera, Aparajita Biswas</p>
-                </div>
-        );
+class  App extends Component {
+        render(){
+                return(
+                        <Container className="text-center">
+                                <h1>Online Document Tracking System</h1>
+                                <p>Developed by: Souradip Nath, Soumili Bera, Aparajita Biswas</p>
+                        </Container>
+                );
+        }
 }
 
 export default App;
