f3c2625e5b210b6e41ff6de5a5e39f273ffc4aa2
confusedDip <anihero4@gmail.com>
2021-11-23 13:00:44 +0530

    Forgot Password Design Done

--- controllers/home.js
+++ controllers/home.js
@@ -3,3 +3,15 @@ import path from 'path';
 export const getHome = function(req, res){
         res.render(path.resolve('./views/home.ejs'));
 }
+
+export const getForgotPassword1 = function(req, res){
+        res.render(path.resolve('./views/forgot-pwd-1.ejs'));
+}
+
+export const getForgotPassword2 = function(req, res){
+        res.render(path.resolve('./views/forgot-pwd-2.ejs'));
+}
+
+export const getForgotPassword3 = function(req, res){
+        res.render(path.resolve('./views/forgot-pwd-3.ejs'));
+}
--- routes/home.js
+++ routes/home.js
@@ -1,8 +1,11 @@
 import express from 'express';
-import {getHome} from '../controllers/home.js';
+import {getHome, getForgotPassword1, getForgotPassword2, getForgotPassword3} from '../controllers/home.js';
 
 const router = express.Router();
 
 router.get("/", getHome);
+router.get("/forgot-password-1", getForgotPassword1);
+router.get("/forgot-password-2", getForgotPassword2);
+router.get("/forgot-password-3", getForgotPassword3);
 
 export default router;
